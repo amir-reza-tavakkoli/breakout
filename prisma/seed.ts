73884ead5e1e27095278e76dbd0de61dd1361076
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-07 11:14:26 +0430

    add seed changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,91 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    
+    await prisma.category.create({
+        data: {
+            name: "Fast food",
+            description: "Really bad",
+            avatarUrl: "",
+            status: {
+                connectOrCreate: {
+                    where: {
+                        overall: "Detrimental",
+                    },
+                    create: {
+                        overall: "Detrimental",
+                        advice: "Eat less of it",
+                    },
+                },
+            },
+        },
+    });
+
+    await prisma.category.create({
+        data: {
+            name: "Fruit",
+            description: "Generally, good",
+            avatarUrl: "",
+            status: {
+                connectOrCreate: {
+                    where: {
+                        overall: "Beneficial",
+                    },
+                    create: {
+                        overall: "Beneficial",
+                        advice: "Take it more frequently",
+                    },
+                },
+            },
+        },
+    });
+
+    await prisma.food.create({
+        data: {
+            name: "Apple",
+            avatarUrl: "",
+            description: "Smells good and benefits your guts",
+            status: {
+                connectOrCreate: {
+                    where: {
+                        overall: "Beneficial",
+                    },
+                    create: {
+                        overall: "Beneficial",
+                        advice: "Take it more frequently",
+                    },
+                },
+            },
+            categories: {
+                create: {
+                    category: {
+                        connect: { name: "Fruit" },
+                    },
+                    isMain: true,
+                },
+            },
+        },
+    });
+
+    await prisma.food.create({
+        data: {
+            name: "Pizza",
+            avatarUrl: "",
+            description: "Contains cheese",
+            status: {
+                connect: {
+                    overall: "Detrimental",
+                },
+            },
+            categories: {
+                create: {
+                    category: {
+                        connect: { name: "Fast food" },
+                    },
+                    isMain: true,
+                },
+            },
+        },
+    });
 }
 
 main()
