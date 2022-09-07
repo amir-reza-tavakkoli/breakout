9efe1660e5210233e95d80fc509894c7a5a2aa6b
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-07 11:14:51 +0430

    format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,99 +2,99 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.category.create({
-        data: {
-            name: "Fast food",
-            description: "Really bad",
-            avatarUrl: "",
-            status: {
-                connectOrCreate: {
-                    where: {
-                        overall: "Detrimental",
-                    },
-                    create: {
-                        overall: "Detrimental",
-                        advice: "Eat less of it",
-                    },
-                },
-            },
+  await prisma.category.create({
+    data: {
+      name: "Fast food",
+      description: "Really bad",
+      avatarUrl: "",
+      status: {
+        connectOrCreate: {
+          where: {
+            overall: "Detrimental",
+          },
+          create: {
+            overall: "Detrimental",
+            advice: "Eat less of it",
+          },
         },
-    });
+      },
+    },
+  });
 
-    await prisma.category.create({
-        data: {
-            name: "Fruit",
-            description: "Generally, good",
-            avatarUrl: "",
-            status: {
-                connectOrCreate: {
-                    where: {
-                        overall: "Beneficial",
-                    },
-                    create: {
-                        overall: "Beneficial",
-                        advice: "Take it more frequently",
-                    },
-                },
-            },
+  await prisma.category.create({
+    data: {
+      name: "Fruit",
+      description: "Generally, good",
+      avatarUrl: "",
+      status: {
+        connectOrCreate: {
+          where: {
+            overall: "Beneficial",
+          },
+          create: {
+            overall: "Beneficial",
+            advice: "Take it more frequently",
+          },
         },
-    });
+      },
+    },
+  });
 
-    await prisma.food.create({
-        data: {
-            name: "Apple",
-            avatarUrl: "",
-            description: "Smells good and benefits your guts",
-            status: {
-                connectOrCreate: {
-                    where: {
-                        overall: "Beneficial",
-                    },
-                    create: {
-                        overall: "Beneficial",
-                        advice: "Take it more frequently",
-                    },
-                },
-            },
-            categories: {
-                create: {
-                    category: {
-                        connect: { name: "Fruit" },
-                    },
-                    isMain: true,
-                },
-            },
+  await prisma.food.create({
+    data: {
+      name: "Apple",
+      avatarUrl: "",
+      description: "Smells good and benefits your guts",
+      status: {
+        connectOrCreate: {
+          where: {
+            overall: "Beneficial",
+          },
+          create: {
+            overall: "Beneficial",
+            advice: "Take it more frequently",
+          },
         },
-    });
+      },
+      categories: {
+        create: {
+          category: {
+            connect: { name: "Fruit" },
+          },
+          isMain: true,
+        },
+      },
+    },
+  });
 
-    await prisma.food.create({
-        data: {
-            name: "Pizza",
-            avatarUrl: "",
-            description: "Contains cheese",
-            status: {
-                connect: {
-                    overall: "Detrimental",
-                },
-            },
-            categories: {
-                create: {
-                    category: {
-                        connect: { name: "Fast food" },
-                    },
-                    isMain: true,
-                },
-            },
+  await prisma.food.create({
+    data: {
+      name: "Pizza",
+      avatarUrl: "",
+      description: "Contains cheese",
+      status: {
+        connect: {
+          overall: "Detrimental",
         },
-    });
+      },
+      categories: {
+        create: {
+          category: {
+            connect: { name: "Fast food" },
+          },
+          isMain: true,
+        },
+      },
+    },
+  });
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
