afb3c74db80a661e0de32cbc973de423ae077ff4
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-08 16:51:43 +0430

    format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,105 +2,105 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.food.create({
-        data: {
-            name: "Fast food",
-            description: "Really bad",
-            avatarUrl: "",
-            status: {
-                connectOrCreate: {
-                    where: {
-                        overall: "Detrimental",
-                    },
-                    create: {
-                        overall: "Detrimental",
-                        advice: "Eat less of it",
-                    },
-                },
-            },
-            successor: {
-                create: {
-                    predecessor: {
-                        create: {
-                            name: "Pizza",
-                            isCategory: false,
-                            avatarUrl: "",
-                            description: "Contains cheese",
-                            status: {
-                                connect: {
-                                    overall: "Detrimental",
-                                },
-                            },
-                        },
-                    },
-                },
-            },
+  await prisma.food.create({
+    data: {
+      name: "Fast food",
+      description: "Really bad",
+      avatarUrl: "",
+      status: {
+        connectOrCreate: {
+          where: {
+            overall: "Detrimental",
+          },
+          create: {
+            overall: "Detrimental",
+            advice: "Eat less of it",
+          },
         },
-    });
-    await prisma.food.create({
-        data: {
-            name: "Fruit",
-            description: "Generally, good",
-            avatarUrl: "",
-            status: {
-                connectOrCreate: {
-                    where: {
-                        overall: "Beneficial",
-                    },
-                    create: {
-                        overall: "Beneficial",
-                        advice: "Take it more frequently",
-                    },
+      },
+      successor: {
+        create: {
+          predecessor: {
+            create: {
+              name: "Pizza",
+              isCategory: false,
+              avatarUrl: "",
+              description: "Contains cheese",
+              status: {
+                connect: {
+                  overall: "Detrimental",
                 },
+              },
             },
-            successor: {
-                create: {
-                    predecessor: {
-                        create: {
-                            name: "Apple",
-                            isCategory: false,
-                            avatarUrl: "",
-                            description: "Full of vitamins",
-                            status: {
-                                connect: {
-                                    overall: "Beneficial",
-                                },
-                            },
-                        },
-                    },
-                },
-            },
+          },
         },
-    });
+      },
+    },
+  });
+  await prisma.food.create({
+    data: {
+      name: "Fruit",
+      description: "Generally, good",
+      avatarUrl: "",
+      status: {
+        connectOrCreate: {
+          where: {
+            overall: "Beneficial",
+          },
+          create: {
+            overall: "Beneficial",
+            advice: "Take it more frequently",
+          },
+        },
+      },
+      successor: {
+        create: {
+          predecessor: {
+            create: {
+              name: "Apple",
+              isCategory: false,
+              avatarUrl: "",
+              description: "Full of vitamins",
+              status: {
+                connect: {
+                  overall: "Beneficial",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 
-    // await prisma.food.create({
-    //   data: {
-    //     name: "Pizza",
-    //     avatarUrl: "",
-    //     description: "Contains cheese",
-    //     status: {
-    //       connect: {
-    //         overall: "Detrimental",
-    //       },
-    //     },
-    //     // categories: {
-    //     //   create: {
-    //     //     category: {
-    //     //       connect: { name: "Fast food" },
-    //     //     },
-    //     //     isMain: true,
-    //     //   },
-    //     // },
-    //   },
-    // });
+  // await prisma.food.create({
+  //   data: {
+  //     name: "Pizza",
+  //     avatarUrl: "",
+  //     description: "Contains cheese",
+  //     status: {
+  //       connect: {
+  //         overall: "Detrimental",
+  //       },
+  //     },
+  //     // categories: {
+  //     //   create: {
+  //     //     category: {
+  //     //       connect: { name: "Fast food" },
+  //     //     },
+  //     //     isMain: true,
+  //     //   },
+  //     // },
+  //   },
+  // });
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
