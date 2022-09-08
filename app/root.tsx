c18847131b34c4b41e2aa5ffa948f53f0f56ea83
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-08 16:31:08 +0430

    import components in root

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
+    Link,
     Links,
     LiveReload,
     Meta,
@@ -8,39 +9,48 @@ import {
     ScrollRestoration,
 } from "@remix-run/react";
 
+import icon from "~/../public/favicon.png";
 import stylesUrl from "~/styles/root.css";
 import base from "~/../node_modules/sanitize.css/sanitize.css";
+import homeNavStyles from "~/styles/home-nav.css";
+import searchBarStyles from "~/styles/search-bar.css";
+
+import { HomeNav } from "./components/home-nav";
+import { SearchBar } from "./components/search-bar";
 
 export const links: LinksFunction = () => {
-    return [{ rel: "stylesheet", href: stylesUrl }, {rel : "stylesheet", href : base}]
+    return [
+        { rel: "stylesheet", href: stylesUrl },
+        { rel: "stylesheet", href: base },
+        { rel: "stylesheet", href: homeNavStyles },
+        { rel: "stylesheet", href: searchBarStyles },
+    ];
 };
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+    charset: "utf-8",
+    title: "Breakout",
+    viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
-  return (
-      <html lang="en">
-          <head>
-              <Meta />
-              <Links />
-              <link
-                  rel="shortcut icon"
-                  href="favicon.png"
-                  type="image/x-icon"
-              />
-          </head>
-          <body>
-              <main className="main">
-                  <Outlet />
-                  <ScrollRestoration />
-                  <Scripts />
-                  <LiveReload />
-              </main>
-          </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <Meta />
+                <Links />
+                <link rel="icon" href={icon} type="image/x-icon" />
+            </head>
+            <body>
+                <main className="main">
+                    <HomeNav />
+                    <Outlet />
+                    <ScrollRestoration />
+                    <Scripts />
+                    <LiveReload />
+                    <SearchBar />
+                </main>
+            </body>
+        </html>
+    );
 }
