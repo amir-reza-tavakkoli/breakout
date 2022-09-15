fb64ade321ca147edb8c30ccde6ec874446ce738
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-15 22:27:12 +0430

    rename to items

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -4,6 +4,5 @@ import { db } from "~/utils/db.server";
 
 export const loader: LoaderFunction = async () => {
   const foods = await db.food.findMany({ select: { name: true } });
-  foods.map((item) => item.name);
-  return json(foods);
+  return json(foods.map((item) => item.name));
 };
