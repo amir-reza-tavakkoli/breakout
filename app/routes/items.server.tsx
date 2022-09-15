f12505a5436b025255922a40f7ff7f8ded68a06b
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-15 21:30:04 +0430

    add items server route

---
+++ app/routes/items.server.tsx
@@ -0,0 +1,8 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const loader: LoaderFunction = async () => {
+    const foods = await db.food.findMany({ select: { name: true } });
+    foods.map((item) => item.name);
+    return json(foods);
+};
