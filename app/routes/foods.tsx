bb9fb81172b6178b8cde983d0452e5dda49754c5
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-15 21:30:52 +0430

    add new routes

--- app/routes/foods.tsx
+++ app/routes/foods.tsx
@@ -1,15 +1,80 @@
-import { MetaFunction } from "@remix-run/node";
-import { Outlet, Link } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { Food } from "@prisma/client";
 
-export const meta: MetaFunction = () => ({
-    title: "Hi",
-});
+import stylesUrl from "~/styles/foods.css";
+import { db } from "~/utils/db.server";
+import { LinksFunction } from "@remix-run/react/dist/routeModules";
+
+export const links: LinksFunction = () => {
+    return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+// type LoaderData = {
+//     foods: Array<{name:string,avatarUrl : string,statusId:string} > | null;
+// };
+type LoaderData = {
+    foods: Array<Pick<Food, "name" | "avatarUrl" | "statusId">> | null;
+};
+
+type LoaderDataExtended = {
+    foods: Array<
+        Pick<Food, "name" | "avatarUrl" | "statusId"> & {
+            status: string;
+        }
+    > | null;
+};
+
+export const loader: LoaderFunction = async () => {
+    const data: LoaderData = {
+        foods: await db.food.findMany({
+            take: 6,
+            select: { name: true, statusId: true, avatarUrl: true },
+        }),
+    };
+
+    if (!data.foods) {
+        throw new Error("no food");
+    }
+
+    const withStatus = await Promise.all(
+        data.foods.map(async (item) => {
+            return {
+                name: item.name,
+                avatarUrl: item.avatarUrl,
+                statusId: item.statusId,
+                status:
+                    (
+                        await db.status.findUnique({
+                            where: { id: item.statusId },
+                        })
+                    )?.overall ?? "Unknown",
+            };
+        })
+    );
+    return json({ foods: withStatus });
+};
 
 export default function Foods() {
+    const data = useLoaderData<LoaderDataExtended>();
+    console.log(data);
     return (
-        <div>
-            <h1>Foods</h1>
-            <Outlet/>
-        </div>
+        <ul className="list">
+            {data.foods &&
+                data.foods.map((food, index) => (
+                    <li key={index}>
+                        <Link to={"/food" + "/" + food.name}>
+                            <span>
+                                {food.avatarUrl ? (
+                                    <img src={food.avatarUrl} alt="" />
+                                ) : null}
+                                <span className="name">{food.name}</span>
+                            </span>
+                            <span className="overall">{food.status}</span>
+                        </Link>
+                    </li>
+                ))}
+        </ul>
     );
 }
