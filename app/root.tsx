814de58780e1bf1187d9e48844809e7e5f654ec1
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-09-15 21:29:52 +0430

    remove bad loader

--- app/root.tsx
+++ app/root.tsx
@@ -15,19 +15,21 @@ import {
     useActionData,
 } from "@remix-run/react";
 
-import icon from "~/../public/favicon.png";
-import stylesUrl from "~/styles/root.css";
-import base from "~/../node_modules/sanitize.css/sanitize.css";
-import homeNavStyles from "~/styles/home-nav.css";
-import searchBarStyles from "~/styles/search-bar.css";
-
 import { HomeNav } from "./components/home-nav";
 import { SearchBar } from "./components/search-bar";
+
+import stylesUrl from "~/styles/root.css";
+import rootStyles from "~/../node_modules/sanitize.css/sanitize.css";
+import homeNavStyles from "~/styles/home-nav.css";
+import searchBarStyles from "~/styles/search-bar.css";
+import icon from "~/../public/favicon.png";
+
+export const LOCALSTORGAE_PREFIX = "liked-item-";
 
 export const links: LinksFunction = () => {
     return [
         { rel: "stylesheet", href: stylesUrl },
-        { rel: "stylesheet", href: base },
+        { rel: "stylesheet", href: rootStyles },
         { rel: "stylesheet", href: homeNavStyles },
         { rel: "stylesheet", href: searchBarStyles },
     ];
@@ -36,23 +38,14 @@ export const links: LinksFunction = () => {
 export const action: ActionFunction = async ({ request }) => {
     const form = await request.formData();
     const name = form.get("search");
-    // const content = form.get("content");
-    // we do this type check to be extra sure and to make TypeScript happy
-    // we'll explore validation next!
-    // if (typeof name !== "string" || typeof content !== "string") {
-    //     throw new Error(`Form not submitted correctly.`);
-    // }
-
-    // const fields = { name, content };
-    console.log(name);
-
-    // const joke = await db.joke.create({ data: fields });
     return redirect(`/food/${name}`);
 };
 
 export const meta: MetaFunction = () => ({
     charset: "utf-8",
     title: "Breakout",
+    description: "What foods can cause you to break out!",
+    autor: "Amir Reza Tavakoli",
     viewport: "width=device-width,initial-scale=1",
 });
 
@@ -66,15 +59,15 @@ export default function App() {
                 <link rel="icon" href={icon} type="image/x-icon" />
             </head>
             <body>
+                <Scripts />
                 <main className="main">
                     <HomeNav />
                     <Outlet />
                     <ScrollRestoration />
                     <LiveReload />
                 </main>
-                    <SearchBar />
+                <SearchBar />
             </body>
-            <Scripts />
         </html>
     );
 }
